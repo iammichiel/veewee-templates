54f4b76c734a4f9e2f7f16612c50297fda929315
Michiel Missotten <michiel@lil-web.fr>
2014-09-16 10:36:35 +0200

    Upgrading disk space.

--- definitions/CentOS-6.5-x86_64-minimal/base.sh
+++ definitions/CentOS-6.5-x86_64-minimal/base.sh
@@ -10,6 +10,7 @@ enabled=1
 gpgcheck=0
 EOM
 
+yum -y update
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
 
 # Make ssh faster by not waiting on DNS
